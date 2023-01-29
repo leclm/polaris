7e7f7386b84d54e2afbf259cc7875dc51a1f0927
Leticia <lelima039@gmail.com>
2023-01-29 16:57:18 -0300

    Implementa tela de Login

---
+++ polaris/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+export const AuthRoutes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  { 
+    path: '', redirectTo: '/login', pathMatch: 'full' 
+  },
+  { 
+    path: '**', redirectTo: '/login', pathMatch: 'full' 
+  }
+];
