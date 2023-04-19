45f2d9e885e1828adc0a0dc04de7a9c05a1eb91b
Katiana Zeitz <katianamzeitz@gmail.com>
2023-04-18 21:16:06 -0300

    Adiciona validação no post e put conteiner

--- polaris-back/Polaris.Conteiner/Repository/ConteinerRepository.cs
+++ polaris-back/Polaris.Conteiner/Repository/ConteinerRepository.cs
@@ -15,8 +15,8 @@ namespace Polaris.Conteiner.Repository
         public IEnumerable<Models.Conteiner> GetConteineresCompleto()
         {
             return Get()
-                .Include(x => x.CategoriaConteiner)
-                .Include(x => x.TipoConteiner);
+                .Include(x => x.CategoriaConteiner.CategoriaConteinerUuid)
+                .Include(x => x.TipoConteiner.TipoConteinerUuid);
         }
 
         public IEnumerable<Models.Conteiner> GetConteineresAtivosCompleto()
