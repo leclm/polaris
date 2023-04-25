4a9b3fb4a00f4344930be96c4533c138bc020ca0
leclm <lelima039@gmail.com>
2023-04-25 12:51:24 -0300

    feat: finaliza CRUD de Categoria, Tipo e Terceirizado

--- polaris/src/app/gerente/editar-categoria/editar-categoria.component.ts
+++ polaris/src/app/gerente/editar-categoria/editar-categoria.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { GerenteService } from '../services';
 import { ActivatedRoute } from '@angular/router';
+import { Categoria } from 'src/app/models/categoria.model';
+import { HttpResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-categoria',
@@ -8,28 +11,39 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./editar-categoria.component.scss']
 })
 export class EditarCategoriaComponent implements OnInit {
-  public status!: string;
-  public categoriaUuid: any;
+  public statusMsg!: string;
+  public categoriaConteinerUuid: any;
   public categoriaData: any;
 
+  public categoria: Categoria = {
+    categoriaConteinerUuid: '',
+    nome: '',
+    status: true
+  }
+
+  @ViewChild("formCategoria") formCategoria!: NgForm;
   constructor( private gerenteService: GerenteService, private activatedRoute: ActivatedRoute ) { }
 
   ngOnInit(): void {
-    this.categoriaUuid = this.activatedRoute.snapshot.params['id'];
-    
-    this.gerenteService.getAllCategorias().subscribe( (res: any) => {
-        this.categoriaData = res;
-      }
-    )
+    this.categoriaConteinerUuid = this.activatedRoute.snapshot.params['id'];
+    this.gerenteService.getAllCategorias().subscribe( (res: any) => { this.categoriaData = res; });
+    this.gerenteService.getCategoriaById(this.categoriaConteinerUuid).subscribe( (res: any) => { this.categoria = res; });
   }
 
-  // mock para mensagem
-  editar(): any {
-    var code = "200";
-    if (code == "200") {
-      this.status = 'success';
-    } else {
-      this.status = 'fail';
-    }
-  }
+  editar() {
+    this.gerenteService.editarCategoria(this.categoria).subscribe(
+      (response: HttpResponse<Categoria>) => {   
+        if (response.status === 200 || response.status === 201) {
+          this.statusMsg = 'success';
+          console.log('Put request successful');
+        } else {
+          console.log('Put request failed');
+        }
+      },
+      (error) => {
+        console.error('Error:', error);
+        this.statusMsg = 'fail';
+      }
+    );
+  };
 }
