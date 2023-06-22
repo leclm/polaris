ceba036d799d60243d97b55505cb271d0f63d3e5
leclm <lelima039@gmail.com>
2023-06-21 21:02:18 -0300

    refact: remove tela de histórico de serviço do terceririzado

--- polaris/src/app/gerente/index.ts
+++ polaris/src/app/gerente/index.ts
@@ -10,7 +10,6 @@ export * from './manter-terceirizado';
 export * from './editar-terceirizado';
 export * from './visualizar-terceirizados';
 export * from './visualizar-detalhes-terceirizado';
-export * from './visualizar-historico-terceirizado';
 
 export * from './manter-categoria';
 export * from './visualizar-categorias';
